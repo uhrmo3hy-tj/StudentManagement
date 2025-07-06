de17e687eeb79ea9f468251d082ecf8a01e243f8
uehara <m19981108@gmail.com>
2025-07-06 15:50:27 +0900

    スタンダード課題11

--- src/main/java/raisetech/StudentManagement/StudentmanagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentmanagementApplication.java
@@ -1,39 +1,19 @@
 package raisetech.StudentManagement;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentRepository;
 
 @SpringBootApplication
-@RestController
 public class StudentmanagementApplication {
-
-  @Autowired
-  private StudentRepository repository;
-
 
   public static void main(String[] args) {
     SpringApplication.run(StudentmanagementApplication.class, args);
   }
-
-  @GetMapping("/studentList")
-  public List <Student> getStudentList(){
-    return repository.search();
-  }
-
-  @GetMapping("/studentCoursesList")
-  public List<StudentsCourses>getStudentsCourseList(){
-    return repository.searchStudentsCourses();
-}
 }
