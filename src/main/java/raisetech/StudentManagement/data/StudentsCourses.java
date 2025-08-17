c00f760e3a20d436260662a9f3c6b6829e65899e
uehara <m19981108@gmail.com>
2025-08-18 07:29:42 +0900

    #16 受講生登録処理の実装

--- src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
+++ src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
@@ -9,53 +9,11 @@ import lombok.Setter;
 public class StudentsCourses {
 
   private String id;
-kadai14
   private String studentId;
-
   private String studentID;
- master
   private String courseName;
   private LocalDateTime startDate;
   private LocalDateTime endDate;
 
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getStudentId() {
-    return studentId;
-  }
-
-  public void setStudentId(String studentId) {
-    this.studentId = studentId;
-  }
-
-  public String getCourseName() {
-    return courseName;
-  }
-
-  public void setCourseName(String courseName) {
-    this.courseName = courseName;
-  }
-
-  public LocalDateTime getStartDate() {
-    return startDate;
-  }
-
-  public void setStartDate(LocalDateTime startDate) {
-    this.startDate = startDate;
-  }
-
-  public LocalDateTime getEndDate() {
-    return endDate;
-  }
-
-  public void setEndDate(LocalDateTime endDate) {
-    this.endDate = endDate;
-  }
 }
 
